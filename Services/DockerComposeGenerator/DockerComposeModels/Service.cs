84fbdac3508bba193463c4136a45a3e0faf37dcd
deepika-soliton <deepika.karthikeyan@solitontech.local>
2023-11-28 10:48:29 +0530

    Modify property order

--- Services/DockerComposeGenerator/DockerComposeModels/Service.cs
+++ Services/DockerComposeGenerator/DockerComposeModels/Service.cs
@@ -3,20 +3,22 @@ using YamlDotNet.Serialization;
 namespace DockerComposeGenerator;
 
 public class Service
-{
+{   
+    public List<string> Profiles { get; set; } = new List<string>();
     public string Image { get; set; } = string.Empty;
     public Build Build { get; set; } = new Build();
-    public List<string> Volumes {get;set;} = new List<string>();
-    public List<string> Profiles { get; set; } = new List<string>();
-    public List<string> Ports { get; set; } = new List<string>();
+
+    [YamlMember(Alias = "container_name")]
+    public string ContainerName { get; set; } = string.Empty;
     public Dictionary<string, string> Environment { get; set; } = new Dictionary<string, string>();
+    public List<string> Ports { get; set; } = new List<string>();
+    public List<string> Volumes {get;set;} = new List<string>();
     public List<string> Networks { get; set; } = new List<string>();
     public string Command { get; set; } = string.Empty;
 
     [YamlMember(Alias = "depends_on")]
     public List<string> DependsOn { get; set; } = new List<string>();
     public RestartPolicy Restart { get; set; } 
-    public string ContainerName { get; set; } = string.Empty;
 
     [YamlMember(Alias = "hostname")]
     public string HostName { get; set; } = string.Empty;
